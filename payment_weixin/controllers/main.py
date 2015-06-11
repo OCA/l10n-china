9f291fba89db0c2bfaf15d4e96040cb467beaa7b
Jeffery Chen Fan <jeffery9@gmail.com>
2015-06-11 16:08:27 +0800

    release Weixin payment beta version

--- payment_weixin/controllers/main.py
+++ payment_weixin/controllers/main.py
@@ -9,6 +9,8 @@ import pprint
 import urllib2
 import werkzeug
 
+from lxml import etree
+
 from openerp import http, SUPERUSER_ID
 from openerp.http import request
 from openerp.addons.payment_weixin.models import util
@@ -18,21 +20,22 @@ _logger = logging.getLogger(__name__)
 
 class WeixinController(http.Controller):
     _notify_url = '/payment/weixin/notify/'
-    _return_url = '/payment/weixin/return/'
-
 
     def weixin_validate_data(self, **post):
         cr, uid, context = request.cr, request.uid, request.context
-        _KEY = request.registry['payment.acquirer']._get_weixin_partner_key()
-        _, prestr = util.params_filter(post)
+        json = {}
+        for el in etree.fromstring(post):
+            json[el.tag] = el.text
+
+        _KEY = request.registry['payment.acquirer']._get_weixin_key()
+        _, prestr = util.params_filter(json)
         mysign = util.build_mysign(prestr, _KEY, 'MD5')
-        if mysign != post.get('sign'):
+        if mysign != json.get('sign'):
             return 'false'
 
         _logger.info('weixin: validated data')
-        return request.registry['payment.transaction'].form_feedback(cr, SUPERUSER_ID, post, 'weixin',
+        return request.registry['payment.transaction'].form_feedback(cr, SUPERUSER_ID, json, 'weixin',
                                                                      context=context)
-
 
     @http.route('/payment/weixin/notify', type='http', auth='none', methods=['POST'])
     def weixin_notify(self, **post):
@@ -42,13 +45,3 @@ class WeixinController(http.Controller):
             return 'success'
         else:
             return ''
-
-    @http.route('/payment/weixin/return', type='http', auth="none", methods=['GET'])
-    def weixin_return(self, **get):
-        """ weixin Return """
-        _logger.info('Beginning weixin return form_feedback with post data %s', pprint.pformat(get))  # debug
-        res = request.registry['payment.transaction'].form_feedback(cr, SUPERUSER_ID, get, 'weixin', context=context)
-        if res:
-            return 'success'
-        return ''
-
