763260d3cc91c2ea7c42a45482ea2e7653b3c634
noah <234082230@qq.com>
2016-08-19 14:35:47 +0800

    [ADD] connector_dns

---
+++ connector_dns/unit/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import backend_adapter
+from . import binder
+from . import export_synchronizer
