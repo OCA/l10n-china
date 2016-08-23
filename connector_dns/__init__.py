927c9c2d4768bf8d80afd0708d1edd274d6092d5
noah <234082230@qq.com>
2016-08-23 16:47:11 +0800

    [IMP] fixing travis errors

--- connector_dns/__init__.py
+++ connector_dns/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright 2015 Elico Corp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from . import backend
 from . import connector
-from . import unit
-from . import tests
 from . import dns
+from . import tests
+from . import unit
+
--- connector_dns/connector.py
+++ connector_dns/connector.py
@@ -1,12 +1,27 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015 Elico Corp
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2010-2015 Elico Corp (<http://www.elico-corp.com>)
+#    Authors: Liu Lixia, Augustin Cisterne-Kaas
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 from openerp import models, fields
-from openerp.addons.connector.connector import (Environment,
-                                                install_in_connector)
+from openerp.addons.connector.connector import (Environment)
 from openerp.addons.connector.checkpoint import checkpoint
-
-install_in_connector()
 
 
 def get_environment(session, model_name, backend_id):
