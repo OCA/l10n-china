400cb0bdf9e23eb879199ee57823592f5fa28fca
noah <234082230@qq.com>
2016-08-24 17:40:01 +0800

    [FIX] format

--- connector_dns/dns.py
+++ connector_dns/dns.py
@@ -12,17 +12,29 @@ class DNSBackend(models.Model):
     def _select_version(self):
         return []
 
-    login = fields.Char('Login', help="Provider's login.", required=True)
-    password = fields.Char('Password', help="Provider's password.",
-                           required=True)
+    login = fields.Char(
+        string='Login',
+        help="Provider's login.",
+        required=True
+    )
+    password = fields.Char(
+        string='Password',
+        help="Provider's password.",
+        required=True
+    )
     state = fields.Selection(
         [('draft', 'Draft'), ('done', 'Done'),
-         ('exception', 'Exception')], 'State', default="draft",
-        help='"Confirmed" when the domain has been succesfully created.')
+         ('exception', 'Exception')],
+        string='State',
+        default="draft",
+        help='"Confirmed" when the domain has been succesfully created.'
+    )
     version = fields.Selection(
-        '_select_version', string='Service Provider',
+        selection='_select_version',
+        string='Service Provider',
         help='DNS service provider',
-        required=True)
+        required=True
+    )
 
     @api.multi
     def name_get(self):
@@ -36,10 +48,16 @@ class DNSDomain(models.Model):
     _name = 'dns.domain'
     _inherit = 'dns.binding'
 
-    name = fields.Char('Name', required=True,
-                       help='Domain name without "www",such as"dnspod.cn"')
-    record_ids = fields.One2many('dns.record', 'domain_id',
-                                 String='Subdomains')
+    name = fields.Char(
+        string='Name',
+        required=True,
+        help='Domain name without "www",such as"dnspod.cn"'
+    )
+    record_ids = fields.One2many(
+        comodel_name='dns.record',
+        inverse_name='domain_id',
+        string='Subdomains'
+    )
 
 
 class DNSRecord(models.Model):
@@ -52,18 +70,43 @@ class DNSRecord(models.Model):
     def _type_select_version(self):
         return []
 
-    name = fields.Char('Sub domain', help="host record,such as 'www'",
-                       required=True)
+    name = fields.Char(
+        string='Sub domain',
+        help="host record,such as 'www'",
+        required=True)
     domain_id = fields.Many2one(
-        'dns.domain', string="Domain", domain="[('state','=','done')]",
-        ondelete='cascade', help="Domain which has already confirmed")
-    type = fields.Selection('_type_select_version', string='Record Type')
-    line = fields.Selection('_line_select_version', string='Record Line')
-    value = fields.Text('Value', help="such as IP:200.200.200.200",
-                        required=True)
-    mx_priority = fields.Integer(string='MX priority', help="scope:1-20",
-                                 default=1)
-    ttl = fields.Integer('TTL', default=600, help="scope:1-604800",
-                         required=True)
-    backend_id = fields.Many2one('dns.backend', related='domain_id.backend_id',
-                                 store=True)
+        comodel_name='dns.domain',
+        string="Domain",
+        domain="[('state','=','done')]",
+        ondelete='cascade',
+        help="Domain which has already confirmed"
+    )
+    type = fields.Selection(
+        selection='_type_select_version',
+        string='Record Type'
+    )
+    line = fields.Selection(
+        selection='_line_select_version',
+        string='Record Line'
+    )
+    value = fields.Text(
+        string='Value',
+        help="such as IP:200.200.200.200",
+        required=True
+    )
+    mx_priority = fields.Integer(
+        string='MX priority',
+        help="scope:1-20",
+        default=1
+    )
+    ttl = fields.Integer(
+        string='TTL',
+        default=600,
+        help="scope:1-604800",
+        required=True
+    )
+    backend_id = fields.Many2one(
+        comodel_name='dns.backend',
+        related='domain_id.backend_id',
+        store=True
+    )
