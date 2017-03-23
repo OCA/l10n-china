f36eacdcf43c7e9f263171c312e273519dac08a3
franksongfeng <frank.song@elico-corp.com>
2017-03-23 15:57:45 +0800

    add tests, README.rst

--- l10n_cn_fapiao/__init__.py
+++ l10n_cn_fapiao/__init__.py
@@ -2,4 +2,5 @@
 # © 2016 Elico Corp (www.elico-corp.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 import fapiao
+import fapiao_test
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
