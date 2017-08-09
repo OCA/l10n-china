3808bcd5ac9323ac008ce34198addaa4834be184
Kevin Dong 董树凯 - Elico Corp <dongshuki@163.com>
2017-08-09 11:59:46 +0800

    modify the version number

--- l10n_cn_fapiao/__openerp__.py
+++ l10n_cn_fapiao/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Chinese Fapiao Management',
-    'version': '8.0.1.0.0',
+    'version': '9.0.1.0.0',
     'category': 'Accounting',
     'sequence': 19,
     'summary': 'Chinese Fapiao Management and link with Odoo Invoice',
